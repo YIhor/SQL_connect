0f0094d09ac26c55086b0223e5c6444527ddf4ea
YIhor <122531038+YIhor@users.noreply.github.com>
2023-01-16 09:58:07 +0200

    Update example_Promise.js

--- example_Promise.js
+++ example_Promise.js
@@ -17,15 +17,17 @@ connectAndQuery();
 async function connectAndQuery() {
     const searchEmail = "protools@test.com"
     const id = 585;
-    try {   
+    try {
         const promise = new Promise((resolve, reject) => {
-        const poolConnection =  sql.connect(config)
+            const poolConnection =  sql.connect(config)
             resolve(poolConnection);
       });
       promise.then((value) => {
-        console.log(value); //this value not displayed
-      });    
-        console.log("Reading rows from the Table...");   
+        const resultSet = value.request().query(`SELECT Email, Id, Salt FROM Users WHERE Id = ${id} `)
+        .then(function(result) {
+          console.log(result) 
+       })        
+      });   
     } catch (err) {
         console.error(err.message);
     }
